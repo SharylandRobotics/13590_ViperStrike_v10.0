3a157b89f8addc245cfc58d238207b01c60db67a
Chris Lemoine <chrismlemoine@gmail.com>
2024-11-15 15:58:12 -0600

    FtcRobotController v10.1.0.1

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotHardware.java
@@ -0,0 +1,217 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.IMU;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+
+public class RobotHardware {
+
+    // Declare OpMode members
+    private final LinearOpMode myOpMode; // gain access to methods in the calling OpMode.
+
+    // Define Motor and Servo objects (Make them private so that they CANT be accessed externally)
+    private DcMotor leftFrontDrive = null;
+    private DcMotor leftBackDrive = null;
+    private DcMotor rightFrontDrive = null;
+    private DcMotor rightBackDrive = null;
+    private DcMotor liftDrive = null;
+    private Servo clawPinch = null;
+    private CRServo clawYaw = null;
+    private Servo clawAxial = null;
+    private Servo clawExtension = null;
+
+    // Define Sensor objects (Make them private so that they CANT be accessed externally)
+    private IMU imu = null; // Universal IMU interface
+
+    /*
+    These variables are declared here (as class members) so they can be updated in various methods, but still be
+    displayed by sendTelemetry()
+     */
+    public ElapsedTime runtime = new ElapsedTime();
+
+    public double drivePower;
+    public double strafePower;
+    public double turnPower;
+
+    public double leftFrontPower;
+    public double leftBackPower;
+    public double rightFrontPower;
+    public double rightBackPower;
+
+    public double DRIVE_SPEED;
+    public double STRAFE_SPEED;
+    public double TURN_SPEED;
+    public double LIFT_SPEED;
+
+    public double CLAW_CLOSE;
+    public double CLAW_OPEN;
+    public double CLAW_DOWN;
+    public double CLAW_UP;
+    public double CLAW_IN;
+    public double CLAW_OUT;
+
+    // Define a constructor that allows the OpMode to pass a reference to itself.
+    public RobotHardware(LinearOpMode opmode) {
+        myOpMode = opmode;
+    }
+
+    /**
+     * Initialize all the robots' hardware.
+     * This method must be called ONCE when the OpMode is initialized.
+     * <p>
+     * All the hardware devices are accessed via the hardware map and initialized.
+     */
+    public void init(){
+
+        // Define and initialize ALL installed motors (note: need to use reference to the actual OpMode).
+        leftFrontDrive = myOpMode.hardwareMap.get(DcMotor.class,"left_front_drive");
+        leftBackDrive = myOpMode.hardwareMap.get(DcMotor.class, "left_back_drive");
+        rightFrontDrive = myOpMode.hardwareMap.get(DcMotor.class, "right_front_drive");
+        rightBackDrive = myOpMode.hardwareMap.get(DcMotor.class, "right_back_drive");
+        liftDrive = myOpMode.hardwareMap.get(DcMotor.class, "lift_drive");
+
+        clawPinch = myOpMode.hardwareMap.get(Servo.class, "claw_pinch");
+        clawYaw = myOpMode.hardwareMap.get(CRServo.class, "claw_yaw");
+        clawAxial = myOpMode.hardwareMap.get(Servo.class, "claw_axial");
+        clawExtension = myOpMode.hardwareMap.get(Servo.class, "claw_extension");
+
+        DRIVE_SPEED = 0.6; // Maximum autonomous driving speed for better distance accuracy.
+        STRAFE_SPEED = 0.6; // Maximum autonomous strafing speed for better distance accuracy.
+        TURN_SPEED = 0.4; // Maximum autonomous turning speed for better rotational accuracy.
+        LIFT_SPEED = 1.0; // Maximum lift speed.
+
+        CLAW_CLOSE = 0.0;
+        CLAW_OPEN = 0.6;
+        CLAW_DOWN = 0.0;
+        CLAW_UP = 0.6;
+        CLAW_IN = 0.0;
+        CLAW_OUT = 0.6;
+
+
+        // Define and initialize ALL installed sensors (note: need to use reference to the actual OpMode).
+        imu = myOpMode.hardwareMap.get(IMU.class, "imu");
+
+        /*
+         Define how the hub is mounted on the robot to get the correct Yaw, Pitch, and Roll values. There are two input
+         parameters required to fully specify the orientation. (1) the first parameter specifies the direction of the
+         printed logo on the hub is pointing. (2) the second parameter specifies the direction the USB connector on the
+         hub is pointing. All directions are relative to the robot, and left/right is as-viewed from behind the robot.
+         */
+        IMU.Parameters parameters = new IMU.Parameters(new RevHubOrientationOnRobot(
+                RevHubOrientationOnRobot.LogoFacingDirection.LEFT,
+                RevHubOrientationOnRobot.UsbFacingDirection.UP));
+
+        imu.initialize(parameters);
+
+        /*
+         Most robots need the motors on one side to be reversed to drive forward. The motor reversals shown here are
+         for a "direct drive" robot (the wheels turn in the same direction as the motor shaft). If your robot has
+         additional gear reductions or uses a right-angled drive, it is important to ensure that your motors are turning
+         in the correct direction. So, start out with the reversals here, BUT when you first test your robot, push the
+         left joystick forward and observe the wheels turn. Reverse the direction (flip FORWARD <-> REVERSE) of any
+         wheel that runs backward. Keep testing until ALL the wheels move the robot forward when you push the left
+         joystick forward.
+         */
+        leftFrontDrive.setDirection(DcMotor.Direction.FORWARD);
+        leftBackDrive.setDirection(DcMotor.Direction.FORWARD);
+        rightFrontDrive.setDirection(DcMotor.Direction.REVERSE);
+        rightBackDrive.setDirection(DcMotor.Direction.REVERSE);
+
+        leftFrontDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        leftBackDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        rightFrontDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        rightBackDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        liftDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        // Reset the IMU when initializing the hardware class
+        imu.resetYaw();
+
+        // Wait for the game to start (Display Gyro value while waiting)
+        while (myOpMode.opModeInInit()) {
+            myOpMode.telemetry.addData("Status", "Hardware Initialized");
+            myOpMode.telemetry.addData("\nStarting at", "%7d :%7d :%7d :%7d",
+                    leftFrontDrive.getCurrentPosition(), leftBackDrive.getCurrentPosition(),
+                    rightFrontDrive.getCurrentPosition(), rightBackDrive.getCurrentPosition());
+            myOpMode.telemetry.update();
+        }
+    }
+
+    /**
+     * Calculate the motor powers required to achieve the requested robot motions:
+     * Drive (Axial motion), Strafe (Lateral motion), and Turn (Yaw motion)
+     * Then send these power levels to the motors.
+     *
+     * @param drive     Forward/Reverse driving power (-1.0 to 1.0) +ve is forward
+     * @param strafe    Right/Left driving power (-1.0 to 1.0) +ve is right
+     * @param turn      Right/Left turning power (-1.0 to 1.0) +ve is clockwise
+     */
+    public void driveFieldCentric(double drive, double strafe, double turn) {
+
+        drivePower = drive;
+        strafePower = strafe;
+        turnPower = turn;
+
+        double max;
+        double botHeading = imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.RADIANS);
+
+        // Rotate the movement direction counter to the bot rotation
+        double strafeRotation = strafe * Math.cos(-botHeading) - drive * Math.sin(-botHeading);
+        double driveRotation = strafe * Math.sin(-botHeading) + drive * Math.cos(-botHeading);
+
+        /*
+         Combine the joystick requests for each axis-motion to determine each wheel's power. Set up a variable for each
+         drive wheel to save the power level for telemetry. Denominator is the largest motor power (absolute value) or
+         1. This ensures all the powers maintain the same ratio, but only when at least one is out of the range [-1, 1]
+         */
+        double denominator = Math.max(Math.abs(driveRotation) + Math.abs(strafeRotation) + Math.abs(turn), 1);
+        leftFrontPower = (driveRotation + strafeRotation + turn) / denominator;
+        leftBackPower = (driveRotation - strafeRotation + turn) / denominator;
+        rightFrontPower = (driveRotation - strafeRotation - turn) / denominator;
+        rightBackPower = (driveRotation +  strafeRotation - turn) / denominator;
+
+        // Normalize the values so no wheel power exceeds 100%.
+        // This ensures that the robot maintains the desired motion.
+        max = Math.max(Math.abs(leftFrontPower), Math.abs(leftBackPower));
+        max = Math.max(max, Math.abs(rightFrontPower));
+        max = Math.max(max, Math.abs(rightBackPower));
+
+        if (max > 1.0) {
+            leftFrontPower /= max;
+            leftBackPower /= max;
+            rightFrontPower /= max;
+            rightBackPower /= max;
+        }
+
+        // Use existing function to drive all wheels.
+        setDrivePower(leftFrontPower, leftBackPower, rightFrontPower, rightBackPower);
+    }
+
+    /**
+     * Pass the requested wheel motor power to the appropriate hardware drive motors.
+     *
+     * @param leftFrontWheel    Forward/Reverse driving power (-1.0 to 1.0) +ve is forward
+     * @param leftBackWheel     Forward/Reverse driving power (-1.0 to 1.0) +ve is forward
+     * @param rightFrontWheel   Forward/Reverse driving power (-1.0 to 1.0) +ve is forward
+     * @param rightBackWheel    Forward/Reverse driving power (-1.0 to 1.0) +ve is forward
+     */
+    public void setDrivePower(double leftFrontWheel, double leftBackWheel,
+                              double rightFrontWheel, double rightBackWheel) {
+        //Output the values to the motor drives.
+        leftFrontDrive.setPower(leftFrontWheel);
+        leftBackDrive.setPower(leftBackWheel);
+        rightFrontDrive.setPower(rightFrontWheel);
+        rightBackDrive.setPower(rightBackWheel);
+    }
+
+    public void setClawPosition(double pinch, double yaw, double axial, double extension) {
+        clawPinch.setPosition(pinch);
+        clawYaw.setPower(yaw);
+        clawAxial.setPosition(axial);
+        clawExtension.setPosition(extension);
+    }
+}
