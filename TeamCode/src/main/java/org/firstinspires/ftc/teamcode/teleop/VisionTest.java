7ea3211ca8640e4042445c0c85ef16a4100f7db5
dante <danneallstar5@gmail.com>
2024-11-23 17:49:40 -0600

    test orientation

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/VisionTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/VisionTest.java
@@ -22,45 +22,13 @@
 package org.firstinspires.ftc.teamcode.teleop;
 
 import android.annotation.SuppressLint;
-import android.util.Size;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 import com.qualcomm.robotcore.util.ElapsedTime;
-import org.firstinspires.ftc.robotcore.external.Telemetry;
-import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
 import org.firstinspires.ftc.robotcore.external.navigation.YawPitchRollAngles;
-import org.firstinspires.ftc.vision.VisionPortal;
-import org.firstinspires.ftc.vision.opencv.ColorBlobLocatorProcessor;
-import org.firstinspires.ftc.vision.opencv.ColorRange;
-import org.firstinspires.ftc.vision.opencv.ImageRegion;
-import org.opencv.core.RotatedRect;
 import org.firstinspires.ftc.teamcode.RobotHardware;
-
-import java.util.List;
-
-/*
- * This OpMode illustrates how to use a video source (camera) to locate specifically colored regions
- *
- * Unlike a "color sensor" which determines the color of an object in the field of view, this "color locator"
- * will search the Region Of Interest (ROI) in a camera image, and find any "blobs" of color that match the requested color range.
- * These blobs can be further filtered and sorted to find the one most likely to be the item the user is looking for.
- *
- * To perform this function, a VisionPortal runs a ColorBlobLocatorProcessor process.
- *   The ColorBlobLocatorProcessor process is created first, and then the VisionPortal is built to use this process.
- *   The ColorBlobLocatorProcessor analyses the ROI and locates pixels that match the ColorRange to form a "mask".
- *   The matching pixels are then collected into contiguous "blobs" of pixels.  The outer boundaries of these blobs are called its "contour".
- *   For each blob, the process then creates the smallest possible rectangle "boxFit" that will fully encase the contour.
- *   The user can then call getBlobs() to retrieve the list of Blobs, where each Blob contains the contour and the boxFit data.
- *   Note: The default sort order for Blobs is ContourArea, in descending order, so the biggest contours are listed first.
- *
- * To aid the user, a colored boxFit rectangle is drawn on the camera preview to show the location of each Blob
- * The original Blob contour can also be added to the preview.  This is helpful when configuring the ColorBlobLocatorProcessor parameters.
- *
- * Use Android Studio to Copy this Class, and Paste it into your team's code folder with a new name.
- * Remove or comment out the @Disabled line to add this OpMode to the Driver Station OpMode list
- */
 
 
 @TeleOp(name = "Vision: test", group = "Robot")
